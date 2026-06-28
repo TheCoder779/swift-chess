901ac97423c297e28a1b2398ad6520eccbd7efb2
TheCoder779 <1019858@lcps.org>
2026-06-28 15:29:03 -0400

    doenst work

---
+++ Sources/swift-chess/main.swift
@@ -0,0 +1,5 @@
+struct main {
+    static func main() {
+        print("Hello, World!")
+    }
+}
--- Sources/swift-chess/swift_chess.swift
+++
@@ -1,9 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-@main
-struct swift_chess {
-    static func main() {
-        print("Hello, world!")
-    }
-}
